a741ac7c40342920316c5931d8d0b0c621b05b4f
Stef Horner <stef@tedslittlerobot.com>
2019-03-10 08:28:40 +0000

    add brewfile and caskfile

--- install/homebrew.sh
+++ install/homebrew.sh
@@ -1,4 +1,18 @@
 #! /usr/bin/env bash
 
+# @todo - if brew doesn't exist, install
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew update
+brew upgrade
+
+brew bundle --file=~/.dotfiles/install/Brewfile
+# Any other links from the Brewfile
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+brew bundle --file=~/.dotfiles/install/Caskfile
+
+brew upgrade
+brew cleanup
+
+qlmanage -r
