a5b7cbfd8326dd966bdc5b1133c041fa4e5d3876
Stef Horner <stef@tedslittlerobot.com>
2019-05-22 15:27:25 +0100

    Comment on aliases

--- zsh/alias-general.zsh
+++ zsh/alias-general.zsh
@@ -1,20 +1,37 @@
+
+# {{{ Laravel
+
 alias ads='artisan dump-server'
+alias art='artisan'
 alias console='php console'
 
-alias ctags="`brew --prefix`/bin/ctags"
+# }}}
+
+# {{{ Editor
 
 alias vi='nvim'
 alias vim='nvim'
 alias vzrc='vi ~/.zshrc'
 alias vvrc='vi ~/.config/nvim/init.vim'
-
-# sudo the last command
-alias ffs='sudo $(fc -ln -1)'
-
+alias ctags="`brew --prefix`/bin/ctags"
 alias pstt='pst .'
+
+# }}}
+
+# {{{ PHP / Composer
+
 alias phpu='./vendor/bin/phpunit'
 alias cmpi='composer install'
 alias cmpu='composer update'
 alias cmpr='composer require'
 alias cmpdu='composer dump-autoload'
 alias cmps='composer show --tree'
+
+# }}}
+
+# {{{ Misc
+
+# sudo the last command
+alias ffs='sudo $(fc -ln -1)'
+
+# }}}
