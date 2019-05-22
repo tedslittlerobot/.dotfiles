d34100498c7276702d8328e662375cf65a3bd287
Stef Horner <stef@tedslittlerobot.com>
2019-05-22 15:24:14 +0100

    Add network zsh helper file

---
+++ zsh/network.zsh
@@ -0,0 +1,14 @@
+
+# Upload the given file and get a public URL for it
+function transfer {
+	if [[ "$HOMESLICE_OS" == "osx" ]]; then
+		curl --upload-file ./$1 https://transfer.sh/$1 | pbcopy
+	else
+		curl --upload-file ./$1 https://transfer.sh/$1
+	fi
+}
+
+# Show the entire redirect chain for the given URL
+function redirect-chain {
+	wget --delete-after $1 2>&1 | grep Location:
+}
