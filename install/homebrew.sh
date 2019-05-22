4c37737a343000c903e58b21ce305dfc50696aec
Stef Horner <stef@tedslittlerobot.com>
2019-05-22 14:36:03 +0100

    Add some feedback / comments to homebrew.sh

--- install/homebrew.sh
+++ install/homebrew.sh
@@ -5,12 +5,17 @@ if ! [ -x "$(command -v brew)" ]; then
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+echo 'Running Homebrew Updates'
 brew update
 brew upgrade
 
+echo 'Running Brewfile'
 brew bundle --file=~/.dotfiles/install/Brewfile
 
+echo 'Final Updates'
 brew upgrade
+
+echo 'Cleaning up Homebrew'
 brew cleanup
 
 # Any other links or config setup from the Brewfile
