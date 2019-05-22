6e00607d4828ad92153f4542ef2b75d70518ec87
Stef Horner <stef@tedslittlerobot.com>
2019-05-22 14:40:34 +0100

    Fixes for install script

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 #! /usr/bin/env bash
 
 bash install/homebrew.sh
-bash install/homebrew-packages.sh
 bash install/language-setup.sh
 bash install/php-packages.sh
 bash install/node-modules.sh
 bash install/oh-my-zsh.sh
 
 bash setup/link.sh
-bash setup/zsh.sh
+bash setup/zshrc.sh
 bash setup/dnsmasq.sh
 
 # https://github.com/driesvints/dotfiles
