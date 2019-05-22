a3ee068c2fa7a3dd45a1cd158cc795c52b507fb6
Stef Horner <stef@tedslittlerobot.com>
2019-05-22 14:37:14 +0100

    fix symlinks

--- setup/link.sh
+++ setup/link.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/.config/nvim
 rm -f ~/.config/nvim/init.vim
-ln -S ~/.dotfiles/links/init.vim ~/.config/nvim/init.vim
+ln -s ~/.dotfiles/links/init.vim ~/.config/nvim/init.vim
 
 rm -f ~/.ctags
-ln -S ~/.dotfiles/links/.ctags ~/.ctags
+ln -s ~/.dotfiles/links/.ctags ~/.ctags
