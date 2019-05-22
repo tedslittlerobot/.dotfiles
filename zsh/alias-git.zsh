3f4ea85a52bad21303463f625b9afc6e313ce7fe
Stef Horner <stef@tedslittlerobot.com>
2019-05-22 15:24:05 +0100

    Update alias-git.zsh

--- zsh/alias-git.zsh
+++ zsh/alias-git.zsh
@@ -1,8 +1,6 @@
-
-alias ggpuush='ggpush -u'
-alias gcsm='gc -S -m'
-alias gcas='gc -S --amend'
-alias gcasp='gcas && ggpush'
-alias gitbattle='git shortlog -s -n'
 alias gfap='git fetch --all --prune'
+alias ggpuush='ggpush -u'
+alias gitbattle='git shortlog -s -n'
+
+# Un-does the latest commit, restoring everything to the git stage
 alias gfuck='git reset HEAD~'
